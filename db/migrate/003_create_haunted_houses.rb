a50edef1a8fa980ef298e6f85e4138b05ae98827
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-14 22:16:03 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,7 +2,10 @@
 class CreateHauntedHouses < ActiveRecod::Migration[4.2]
   def change
     create_table :haunted_houses do |t|
-      
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.integer :price
     end
   end
 end
