697a1fb84c73f5ace237c9d879b0e44ed783da7e
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-14 22:15:25 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,5 +1,8 @@
 # Create your haunted_houses migration here
 class CreateHauntedHouses < ActiveRecod::Migration[4.2]
   def change
+    create_table :haunted_houses do |t|
+      
+    end
   end
 end
