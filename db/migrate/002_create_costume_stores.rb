d710917f2fc090d6e3eb6e5bfac592107f887ad8
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-14 22:08:41 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,6 +4,10 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
     create_table :costume_stores do |t|
       t.string :name
       t.string :location
+      t.integer :cosumte_inventory
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      
     end
   end
 end
