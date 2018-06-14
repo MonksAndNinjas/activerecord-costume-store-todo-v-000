7ce1e37c6841d532214a67e147b723a50d0aefcc
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-14 22:05:40 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,4 +1,9 @@
 # Create your costume_stores migration here
 class CreateCostumeStores < ActiveRecord::Migration[4.2]
-
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+    
+  end
 end
